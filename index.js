5ee6db8ba30ff862da07cf4b08e812389a6e8bfe
Marvin <marvin.ogot@gmail.com>
2025-01-16 17:17:06 +0300

    ternary expressions (a shorthand of if else, also done switch statements

--- index.js
+++ index.js
@@ -14,10 +14,36 @@ function scuberGreetingForFeet(distance){
   
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let msg;
+  msg = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return msg;
+}
+  
+
+  if(city === NYC){
+    msg = 'Ok, sounds good.';
+
+  }
+  else if(city === Pittsburgh){
+    msg = 'No go.';
+  }
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  let msgs;
+  switch(tip){
+    case "generous":
+      msgs = "Thank you so much.";
+      break;
+    case "not as generous":
+      msgs = 'Thank you.';
+      break;
+    default:
+      msgs = "Bye."; 
+  }
+  return msgs;
 }
