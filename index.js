4f18884e9169cb0b36b05671bb5ed0f4d982df56
Marvin <marvin.ogot@gmail.com>
2025-01-16 15:06:36 +0300

    partially completed

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400){
+    return 'This one is on me!';
+  }
+  else if(ride > 2000 && ride < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
 function ternaryCheckCity(){
